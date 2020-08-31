3a4410a6cce33c4c63bbd59509513d05b0514574
samuel augusto <samuel_augusto@hotmail.com>
2020-08-31 20:47:07 -0300

    Criando Entidades Ticket, User e ChangeStatus

---
+++ src/main/java/com/samuel/helpdesk/api/enums/PriorityEnum.java
@@ -0,0 +1,9 @@
+package com.samuel.helpdesk.api.enums;
+
+public enum PriorityEnum {
+
+	High,
+	Normal,
+	Low
+	
+}
---
+++ src/main/java/com/samuel/helpdesk/api/enums/ProfileEnum.java
@@ -0,0 +1,9 @@
+package com.samuel.helpdesk.api.enums;
+
+public enum ProfileEnum {
+
+	ROLE_ADMIN,
+	ROLE_CUSTOMER,
+	ROLE_TECHINICIAN
+	
+}
