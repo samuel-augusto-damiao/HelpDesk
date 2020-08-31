9959690527986466a5e17acf7b44e784c58c7a42
samuel augusto <samuel_augusto@hotmail.com>
2020-08-31 19:55:46 -0300

    Projeto Criado

---
+++ src/main/java/com/samuel/helpdesk/HelpTextApplication.java
@@ -0,0 +1,13 @@
+package com.samuel.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpTextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpTextApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samuel/helpdesk/HelpTextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuel.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpTextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
